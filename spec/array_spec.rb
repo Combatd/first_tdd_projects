fd8e71223a2fd17d5d3294fef8528589266c75a0
Combatd <combatdstar@gmail.com>
2020-01-11 18:20:02 -0800

    Describe Array#my_uniq returns new array

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,9 +3,17 @@ require 'array'
 
 
 describe Array do
+  
     
-    describe "#my_uniq" do
-        
+  describe "#my_uniq" do
+    let(:array) { [1, 2, 1, 3, 3] }
+
+    it "removes duplicate elements"
+
+    it "returns a new array" do
+        expect(array.instance_of?(Array)).to eq(true)
     end
+
+  end
 
 end
