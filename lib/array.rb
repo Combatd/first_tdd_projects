ec3bb90ca9c7263783d490db3616385577dd7e11
Combatd <combatdstar@gmail.com>
2020-01-11 18:34:39 -0800

    Define Array#my_uniq to be a class method

--- lib/array.rb
+++ lib/array.rb
@@ -4,8 +4,10 @@ require 'active_support/inflector'
 # monkey-patching Array class
 class Array
 
-    def my_uniq(array)
-        
+    def my_uniq
+        unique_arr = []
+        self.each { |ele| unique_arr << ele if !unique_arr.include?(ele) }
+        unique_arr
     end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -4,11 +4,13 @@ require 'array'
 
 describe Array do
   
-    
+
   describe "#my_uniq" do
     let(:array) { [1, 2, 1, 3, 3] }
 
-    it "removes duplicate elements"
+    it "removes duplicate elements" do
+        expect(array.my_uniq).to eq([1, 2, 3])
+    end
 
     it "returns a new array" do
         expect(array.instance_of?(Array)).to eq(true)
